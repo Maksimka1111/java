8386d7ebc492be13ec143c4c79d9c46713cabca0
Maksimka1111 <92277262+Maksimka1111@users.noreply.github.com>
2022-12-14 13:10:56 +0300

    Delete task11 directory

--- task11/test.java
+++
@@ -1,8 +0,0 @@
-package task11;
-
-public class test {
-    public static void main(String[] args) {
-        Convertation convertation = new Convertation(-40);
-        System.out.println(convertation.convert());
-    }
-}
